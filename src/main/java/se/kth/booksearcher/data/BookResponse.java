5679aaea5eed20bb3dc3e99551cf9d69f166d230
Karpov <34632130+devKarpov@users.noreply.github.com>
2025-04-16 15:07:37 +0200

    Update BookResponse.java

--- src/main/java/se/kth/booksearcher/data/BookResponse.java
+++ src/main/java/se/kth/booksearcher/data/BookResponse.java
@@ -3,6 +3,7 @@ package se.kth.booksearcher.data;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.jetbrains.annotations.NotNull;
 
+// more stuff will be here later
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record BookResponse(
         @NotNull String title,
